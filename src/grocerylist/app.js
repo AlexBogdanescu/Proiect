7f7ce685f8dc0b4c0edd0c53b8cc120d84431190
AlexBogdanescu <97976590+AlexBogdanescu@users.noreply.github.com>
2022-01-20 03:45:45 +0200

    Initial commit

---
+++ src/grocerylist/app.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    const newItem = {
+      id: uuidv4(),
+      item: item,
+      complete: false
+    };
+    e.preventDefault();
+    if (item) {
+      setList([...list, newItem]);
+      setItem("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={item} onChange={handleChange} />
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
